cdcf57200c1b3c8063de78c1cd49f1c98b56a430
Brian Rajchel <60751431+bmrajchel@users.noreply.github.com>
2020-03-05 11:39:22 -0600

    vscode

---
+++ BAC/Data/UserLoginContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BAC.Models;
+
+namespace BAC.Data
+{
+    public class UserLoginContext : DbContext
+    {
+        public UserLoginContext (DbContextOptions<UserLoginContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BAC.Models.Userlogin> Userlogin { get; set; }
+    }
+}
